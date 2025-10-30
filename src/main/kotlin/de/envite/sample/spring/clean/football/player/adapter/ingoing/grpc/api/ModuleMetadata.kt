d3dac4d03ccfcea337713286555174d3d2accc6d
Peter Kutschera <extern.kutschera_peter@allianz.de>
2025-10-30 16:30:22 +0100

    document dependency Kotlin style

---
+++ src/main/kotlin/de/envite/sample/spring/clean/football/player/adapter/ingoing/grpc/api/ModuleMetadata.kt
@@ -0,0 +1,8 @@
+package de.envite.sample.spring.clean.football.player.adapter.ingoing.grpc.api
+
+import org.springframework.modulith.NamedInterface
+import org.springframework.modulith.PackageInfo
+
+@PackageInfo
+@NamedInterface(name = ["grpc-api"])
+class ModuleMetadata {}
