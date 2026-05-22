f37a63df75a1c98cd0da591c30b59345f1352259
exruos <115374714+exruos@users.noreply.github.com>
2026-05-22 02:14:50 +0200

    update versions, fix kotlin compile options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.21"
     kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.1"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
-    id("com.google.protobuf") version "0.9.5"
+    id("com.google.protobuf") version "0.10.0"
 }
 
 group = "de.envite.sample.spring.clean.football"
@@ -26,11 +27,11 @@ repositories {
 }
 
 extra["springGrpcVersion"] = "1.0.0"
-extra["springModulithVersion"] = "2.0.1"
+extra["springModulithVersion"] = "2.0.6"
 
 dependencies {
     implementation("tools.jackson.module:jackson-module-kotlin")
-    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
+    implementation("net.logstash.logback:logstash-logback-encoder:9.0")
     implementation("io.grpc:grpc-services")
     implementation("org.springframework.grpc:spring-grpc-server-web-spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -45,15 +46,15 @@ dependencies {
     runtimeOnly("org.springframework.modulith:spring-modulith-actuator")
     runtimeOnly("org.springframework.modulith:spring-modulith-observability")
 
-    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("com.ninja-squad:springmockk:5.0.1")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.grpc:spring-grpc-test")
     testImplementation("org.springframework.modulith:spring-modulith-starter-test")
-    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
-    testImplementation("org.testcontainers:postgresql:1.21.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.4")
+    testImplementation("org.testcontainers:postgresql:1.21.4")
     testImplementation("org.wiremock:wiremock-standalone:3.13.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
@@ -123,7 +124,8 @@ tasks {
     withType<KotlinCompile> {
         compilerOptions {
             freeCompilerArgs =
-                listOf("-Xjsr305=strict", "-Xjvm-default=all", "-Xannotation-default-target=param.property")
+                listOf("-Xjsr305=strict")
+            jvmDefault.set(JvmDefaultMode.NO_COMPATIBILITY)
             jvmTarget = JvmTarget.JVM_21
             javaParameters = true
             allWarningsAsErrors = true
