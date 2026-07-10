8ca3f58af227e6c873a00d5f7fd49fcd88b81c7e
exruos <115374714+exruos@users.noreply.github.com>
2026-07-10 02:29:51 +0200

    fix teamDtos.kt column mapping

--- src/main/kotlin/de/envite/sample/spring/clean/football/team/adapter/outgoing/persistence/TeamDtos.kt
+++ src/main/kotlin/de/envite/sample/spring/clean/football/team/adapter/outgoing/persistence/TeamDtos.kt
@@ -1,6 +1,7 @@
 package de.envite.sample.spring.clean.football.team.adapter.outgoing.persistence
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("team")
@@ -15,28 +16,28 @@ data class TeamDto(
 @Table("team_attributes")
 data class TeamAttributesDto(
     @Id val id: Int,
-    val teamFifaApiId: Int?,
-    val teamApiId: Int,
+    @Column("team_fifa_api_id") val teamFifaApiId: Int?,
+    @Column("team_api_id") val teamApiId: Int,
     val date: String,
-    val buildUpPlaySpeed: Int?,
-    val buildUpPlaySpeedClass: String?,
-    val buildUpPlayDribbling: Int?,
-    val buildUpPlayDribblingClass: String?,
-    val buildUpPlayPassing: Int?,
-    val buildUpPlayPassingClass: String?,
-    val buildUpPlayPositioningClass: String?,
-    val chanceCreationPassing: Int?,
-    val chanceCreationPassingClass: String?,
-    val chanceCreationCrossing: Int?,
-    val chanceCreationCrossingClass: String?,
-    val chanceCreationShooting: Int?,
-    val chanceCreationShootingClass: String?,
-    val chanceCreationPositioningClass: String?,
-    val defencePressure: Int?,
-    val defencePressureClass: String?,
-    val defenceAggression: Int?,
-    val defenceAggressionClass: String?,
-    val defenceTeamWidth: Int?,
-    val defenceTeamWidthClass: String?,
-    val defenceDefenderLineClass: String?
+    @Column("buildupplayspeed") val buildUpPlaySpeed: Int?,
+    @Column("buildupplayspeedclass") val buildUpPlaySpeedClass: String?,
+    @Column("buildupplaydribbling") val buildUpPlayDribbling: Int?,
+    @Column("buildupplaydribblingclass") val buildUpPlayDribblingClass: String?,
+    @Column("buildupplaypassing") val buildUpPlayPassing: Int?,
+    @Column("buildupplaypassingclass") val buildUpPlayPassingClass: String?,
+    @Column("buildupplaypositioningclass") val buildUpPlayPositioningClass: String?,
+    @Column("chancecreationpassing") val chanceCreationPassing: Int?,
+    @Column("chancecreationpassingclass") val chanceCreationPassingClass: String?,
+    @Column("chancecreationcrossing") val chanceCreationCrossing: Int?,
+    @Column("chancecreationcrossingclass") val chanceCreationCrossingClass: String?,
+    @Column("chancecreationshooting") val chanceCreationShooting: Int?,
+    @Column("chancecreationshootingclass") val chanceCreationShootingClass: String?,
+    @Column("chancecreationpositioningclass") val chanceCreationPositioningClass: String?,
+    @Column("defencepressure") val defencePressure: Int?,
+    @Column("defencepressureclass") val defencePressureClass: String?,
+    @Column("defenceaggression") val defenceAggression: Int?,
+    @Column("defenceaggressionclass") val defenceAggressionClass: String?,
+    @Column("defenceteamwidth") val defenceTeamWidth: Int?,
+    @Column("defenceteamwidthclass") val defenceTeamWidthClass: String?,
+    @Column("defencedefenderlineclass") val defenceDefenderLineClass: String?
 )
